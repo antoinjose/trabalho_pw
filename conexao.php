f8fd4fc105a86cf594ccd03520ba641f6e1e0089
antoinjose <antonio.meireles1@aluno.ce.gov.br>
2026-06-09 19:17:01 +0000

    ok

--- conexao.php
+++ conexao.php
@@ -5,7 +5,6 @@ $username = 'admin';
 $password = '1234';  
 
 try {
-    // IMPORTANTE: Altere de 'mysql:host=localhost' para 'mysql:host=db'
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
